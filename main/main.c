4789b90d7b6f933ec1c4d9fae8921765a42ef3e5
Murphyj1098 <jrmurphy@wpi.edu>
2023-10-01 12:34:25 -0400

    Begin Wi-Fi configuration

--- main/main.c
+++ main/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "sdkconfig.h"
+
+#define WIFI_SSID  CONFIG_ESP_WIFI_SSID
+#define WIFI_PASS  CONFIG_ESP_WIFI_PASS
+#define WIFI_RETRY CONFIG_ESP_MAXIMUM_RETRY
 
 void app_main(void)
 {
